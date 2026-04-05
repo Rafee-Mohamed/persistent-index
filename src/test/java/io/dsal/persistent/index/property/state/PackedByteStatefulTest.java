4b49f941c7c24c2f2f062f58d06c9e26d1c50a54
Mohamed Rafeek Raja S <mohamedrafee2002@gmail.com>
2026-04-05 20:39:41 +0530

    test: reduce PackedByteStatefulTest arbitrary byte array size to 8 to make test faster

--- src/test/java/io/dsal/persistent/index/property/state/PackedByteStatefulTest.java
+++ src/test/java/io/dsal/persistent/index/property/state/PackedByteStatefulTest.java
@@ -23,7 +23,14 @@ public class PackedByteStatefulTest extends AbstractBPlusTreeStatefulTest<byte[]
     protected Arbitrary<byte[]> arbitraryKeys() {
         return Arbitraries.bytes()
                 .array(byte[].class)
-                .ofMaxSize(64)
+                // larger bytes sequence say 4 results in 256^4 unique keys (equal to integer range ~2.17 billion.
+                // If tried 1000 tries for generating values within the range with collision
+                // is mathematically zero (1 / 256^4). If keys never overlap, the tree only grows linearly.
+                // Put will never overwrite. Remove will rarely succeed. But, Stateful engine will be completely blind to
+                // compaction logic for PackedByte trees as we need arbitrary length of bytes to ensure the working of
+                // variable length bytes therefore keeping it as 8 but collision is near zero and map
+                // grows linearly with put.
+                .ofMaxSize(8)
                 .filter(bytes -> bytes.length > 0);
     }
 
